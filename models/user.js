7129a3917ebe02a03d1ffca5ff88b1e4407208df
gopi-chandu <104309886+gopi-chandu@users.noreply.github.com>
2022-07-22 09:03:05 +0530

    multer setup in user schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
+const multer = require("multer");
+const path = require("path");
+const AVATAR_PATH = path.join("/uploads/users/avatars");
+
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -15,6 +19,9 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+    },
   },
   {
     timestamps: true,
@@ -22,5 +29,20 @@ const userSchema = new mongoose.Schema(
 );
 
 const User = mongoose.model("User", userSchema);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+  "avatar"
+);
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 module.exports = User;
