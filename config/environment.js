26748759ab01a054f9e236a639dd037a145f6bea
gopi-chandu <104309886+gopi-chandu@users.noreply.github.com>
2022-07-27 21:46:16 +0530

    enviroment setup

---
+++ config/environment.js
@@ -0,0 +1,21 @@
+
+const production = {
+  name: "production",
+  asset_path: "./assets",
+  session_cookie_key: process.env.secret,
+  db: process.env.db,
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    secure: false,
+    port: process.env.port,
+    auth: {
+      user: process.env.email,
+      // password : ujyvkqzbazbjjwro
+      pass: process.env.password,
+    },
+  },
+  jwtKey: process.env.secret,
+};
+
+module.exports =developemnt;
